f0a4f0db1f1b31e092e0979cf4b9eddab23e70af
PunnaPriyanka <priyankap.mca25@siescoms.sies.edu.in>
2026-03-02 12:21:46 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,5 +3,6 @@ public class hello{
         System.out.println("Hello World!!");
         System.out.println("Addition = " + (2+1));
         System.out.println("Priyanka");
+        System.out.println("Punna");
     }
 }
